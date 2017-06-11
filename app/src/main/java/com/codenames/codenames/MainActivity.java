436de1240dddefde6d9d8fb9926fea0b4b9ea9fb
Sam Greenberg <11greenberg.sam@gmail.com>
2017-06-11 03:31:47 -0400

    Changing gradle min version to 16 because stupid checks are bad

--- app/src/main/java/com/codenames/codenames/MainActivity.java
+++ app/src/main/java/com/codenames/codenames/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         View decorView = getWindow().getDecorView();
-        // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
     }
