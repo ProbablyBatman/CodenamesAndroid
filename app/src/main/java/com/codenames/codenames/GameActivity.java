293265d56d1a0664fe76f8967fbb174d1828ba15
tpg45 <tpg45@scarletmail.rutgers.edu>
2017-06-11 04:18:46 -0400

    trying to make buttons work

--- app/src/main/java/com/codenames/codenames/GameActivity.java
+++ app/src/main/java/com/codenames/codenames/GameActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
