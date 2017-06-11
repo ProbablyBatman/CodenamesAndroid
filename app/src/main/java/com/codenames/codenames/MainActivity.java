940bacd3819ef25a542658736567534ca3eb321b
Sam Greenberg <11greenberg.sam@gmail.com>
2017-06-11 03:30:45 -0400

    Added settings icon and removed action bar and status bar

--- app/src/main/java/com/codenames/codenames/MainActivity.java
+++ app/src/main/java/com/codenames/codenames/MainActivity.java
@@ -1,13 +1,22 @@
 package com.codenames.codenames;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
+
+
 }
